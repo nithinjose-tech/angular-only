8928a4377d500cd07dcf703f25353fc753bec5df
Nithin <suniljose512@gmail.com>
2020-05-23 09:49:30 -0700

    angular component part-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatButtonModule} from '@angular/material/button';   
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 
 
@@ -23,11 +28,14 @@ import { MenuComponent } from './menu/menu.component';
     FlexLayoutModule,
     MatToolbarModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
     
     ],
   
-  . . .
-  ],
+  
+  
   providers: [],
   bootstrap: [AppComponent]
 })
